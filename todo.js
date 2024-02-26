dc5ce2ed1b7df2987e818fa338f64aa5bc36e366
Rushabh Mahawarkar <58899214+Rushabh025@users.noreply.github.com>
2024-02-26 20:05:57 +0530

    updated todo list

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
-//iffi design i.e. wrapping everything in a function so varaibles are not accessible globally.
 (function () {
+    // Task-related data and DOM elements
     let tasks = [];
     const tasksList = document.getElementById('list');
     const addTaskInput = document.getElementById('add');
@@ -7,45 +7,27 @@
 
     console.log('Working');
 
-    async function fetchTodos() {
-        //GET request using fetch promise logic  
-        // fetch('https://jsonplaceholder.typicode.com/todos')
-        //     .then(function(response){
-        //         // console.log(response);
-        //         return response.json();
-        //     }).then(function(data){
-        //         tasks = data.slice(0, 10);
-        //         renderList();
-        //         // console.log(data);
-        //     })
-        //     .catch(function(error){
-        //         console.log('error',error);
-        //     })
-
-        //GET request using async await
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-            const data = await response.json();
-            tasks = data.slice(0, 10);
-            renderList();
-        } catch (error) {
-            console.log(error);
-        }
+    // Function to fetch initial tasks (currently set to an empty array)
+    function fetchTodos() {
+        tasks = []; // Initialize tasks as an empty array
+        renderList();
     }
 
+    // Function to create HTML for a task and append it to the list
     function addTaskToDOM(task) {
         const li = document.createElement('li');
 
         li.innerHTML = `
-    <li>
-        <input type="checkbox" id="${task.id}" ${task.completed ? 'checked' : ''} class = "custom-checkox">
-        <label for="${task.id}">${task.title}</label>
-        <img src="bin.svg" class="delete" data-id="${task.id}" />
-    </li>
-    `;
+            <li>
+                <input type="checkbox" id="${task.id}" ${task.completed ? 'checked' : ''} class="custom-checkbox">
+                <label for="${task.id}">${task.title}</label>
+                <img src="bin.svg" class="delete" data-id="${task.id}" />
+            </li>
+        `;
         tasksList.append(li);
     }
 
+    // Function to render the entire list of tasks
     function renderList() {
         tasksList.innerHTML = '';
 
@@ -56,12 +38,14 @@
         tasksCounter.innerHTML = tasks.length;
     }
 
+    // Function to toggle the completion status of a task
     function toggleTask(taskId) {
         const newTasks = tasks.filter(function (task) {
-            return task.id === Number(taskId)
+            return task.id === Number(taskId);
         });
-        if (taskId.length > 0) {
-            const currentTask = task[0];
+
+        if (newTasks.length > 0) {
+            const currentTask = newTasks[0];
 
             currentTask.completed = !currentTask.completed;
             renderList();
@@ -72,29 +56,33 @@
         showNotification('Could not toggle task');
     }
 
+    // Function to delete a task
     function deleteTask(taskId) {
         const newTasks = tasks.filter(function (task) {
-            return task.id !== Number(taskId)
+            return task.id !== Number(taskId);
         });
         tasks = newTasks;
         renderList();
-        showNotification("Task Deleted");
+        showNotification('Task Deleted');
     }
 
+    // Function to add a new task
     function addTask(task) {
         if (task) {
             tasks.push(task);
             renderList();
-            showNotification("Task Added");
+            showNotification('Task Added');
             return;
         }
-        showNotification("Task cannot be added..!");
+        showNotification('Task cannot be added..!');
     }
 
+    // Function to display a notification (currently using alert)
     function showNotification(text) {
         alert(text);
     }
 
+    // Event handler for key press in the input field
     function handleInputKeyPress(e) {
         if (e.key === 'Enter') {
             const text = e.target.value;
@@ -107,34 +95,39 @@
             const task = {
                 title: text,
                 id: Date.now(),
-                completed: false
-            }
+                completed: false,
+            };
 
             e.target.value = '';
             addTask(task);
         }
     }
 
+    // Event handler for clicks on the document
     function handleClickListener(e) {
         const target = e.target;
 
         if (target.className === 'delete') {
-            const taskId = target.dataset.id
+            const taskId = target.dataset.id;
             deleteTask(taskId);
             return;
         } else if (target.className === 'custom-checkbox') {
-            const taskId = target.id
+            const taskId = target.id;
             toggleTask(taskId);
             return;
         }
     }
 
+    // Function to initialize the application
     function initializeApp() {
-        fetchTodos();
         addTaskInput.addEventListener('keyup', handleInputKeyPress);
         document.addEventListener('click', handleClickListener);
+
+        // Fetch initial tasks (currently set to an empty array)
+        fetchTodos();
     }
 
+    // Initialize the application when the script is loaded
     initializeApp();
 
-})()
+})();
